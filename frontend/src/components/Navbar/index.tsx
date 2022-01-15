964d9c49a3fe2a9f939dff5e7a0a5cf4dad8dc5e
Jair Torezone <jairtorezone@gmail.com>
2022-01-15 14:30:11 -0300

    update Navbar index.tsx

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ function Navbar(){
                 <a href="https://github.com/JairTorezone" target="_blank" rel="noreferrer">
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
-                        <p className="dsmovie-contact-link">/devsuperior</p>
+                        <p className="dsmovie-contact-link">/JairTorezone</p>
                     </div>
                 </a>
             </div>
